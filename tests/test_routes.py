e941d571aad79780826e86931a7a6c0ff1a2797c
gerzech <133556920+gerzech@users.noreply.github.com>
2023-06-27 12:45:55 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -178,6 +178,12 @@ class TestAccountService(TestCase):
             'X-XSS-Protection': '1; mode=block',
             'X-Content-Type-Options': 'nosniff',
             'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
